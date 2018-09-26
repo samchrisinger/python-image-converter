50a015ba77d6f4a83fac51f1e66a311d8678d254
= <=>
2018-09-26 12:28:58 -0400

    Better iteration and logging

--- convert.py
+++ convert.py
@@ -1,21 +1,42 @@
 import os
+import shutil
 import sys
 import rawpy
 import imageio
 from glob import glob
 
+
 def convert():
+  import ipdb; ipdb.set_trace()
   path = sys.argv[1].strip('\'').strip('"')
-  targets = glob(path + '*.nef') + glob(path + '*.raf') + glob(path + '*.NEF') + glob(path + '*.RAF')
-  i = 0
+  targets = os.listdir(path)#(path + '*.nef') + glob(path + '*.raf') + glob(path + '*.NEF') + glob(path + '*.RAF')
+  i = 1
   total = len(targets)
+  failed = []
   for target in targets:
-    print('Attempting to convert {} ({} of {})'.format(target, i, total))
-    with rawpy.imread(target) as raw:
-      rgb = raw.postprocess()
-    base = os.path.splitext(target)[0]
-    print('Saving converted verison of {}'.format(target))
-    imageio.imsave(base + '.jp2', rgb)
+    print("Attempting {} of {}".format(i, total))
+    i += 1
+    filename, file_extension = os.path.splitext(target)
+    if not file_extension.lower() in ('.nef', '.raf'):
+      print('Skipping {}'.format(target))
+      continue
+    if os.path.isfile(filename + '.jp2'):
+      print('Skipping the already converted {}'.format(target))
+      continue
+    print('Attempting to convert {}'.format(target))
+    try:
+      with rawpy.imread(path + target) as raw:
+        rgb = raw.postprocess()
+      base = os.path.splitext(target)[0]
+      print('Saving converted verison of {}'.format(target))
+      imageio.imsave(path + filename + '.jp2', rgb)
+    except:
+      print('Could not convert {}'.format(target))
+      failed.append(target)
+      continue
+  print("------FAILURES-----------")
+  for failure in failed:
+    print(failure)
 
 if __name__ == "__main__":
   convert()
